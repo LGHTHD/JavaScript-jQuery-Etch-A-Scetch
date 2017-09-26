0f4c00979f94c046f789db23a6ba246f13cf8098
William Bjork <williambjrk@gmail.com>
2017-09-26 14:53:41 -0400

    Added grid, added hovering effect that fills the individual grids with black

---
+++ function.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    for(var x = 0; x < 16; x++) {
+        for(var y = 0; y < 16; y++) {
+            var unit = $("<div class='unit'></div>");
+            unit.appendTo('.container');
+        }
+    }
+
+    $('.unit').on('mouseover',function() {
+      $(this).css("background-color", "black");
+
+
+
+
+  });
+});
